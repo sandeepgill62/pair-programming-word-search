e34be57267b0370fd5e2f0a08611cccf704747e8
Sandeep Gill <sandeepgill62@gmail.com>
2023-05-23 05:05:20 +0000

    adding code files with testr

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+
+    //check empty matrix
+    if (letters.length === 0) {
+        return true;
+    }
+
+    // code to search word horizontally
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word))
+            return true
     }
+
+    //transpose the matrix
+    var lettersTranspose = [], col, row;
+    col = letters[0].length;
+    row = letters.length;
+    for (var y = 0; y < col; y++) {
+        lettersTranspose[y] = [];
+        for (var x = 0; x < row; x++) {
+            lettersTranspose[y][x] = letters[x][y];
+        }
+    }
+
+    // code to search word vertically
+    const verticalJoin = lettersTranspose.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word))
+            return true
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
